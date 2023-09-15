c4273d49364d54017140166cbd73d6765c7d2e25
Dedy Yasriady <yasriady@gmail.com>
2023-09-15 10:17:39 +0700

    Mysql config using .env

--- src/config/myconfig.js
+++ src/config/myconfig.js
@@ -1,8 +1,18 @@
+require("dotenv").config()
+// console.log(process.env)
+
+const dbHost = process.env.MYSQL_HOST
+const dbName = process.env.MYSQL_DB
+const dbUser = process.env.MYSQL_USER
+const dbPasswd = process.env.MYSQL_PASSWD
+
 var { Sequelize } = require("sequelize");
-const db = new Sequelize('wea_db', 'wea', 'Fcug3Ci3B7xM6L9r', {
-    host: 'localhost',
+const db = new Sequelize(dbName, dbUser, dbPasswd, {
+    host: dbHost,
     dialect: 'mysql',
     logging: false,
 });
 
 module.exports = db;
+
+
