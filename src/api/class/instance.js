bb55920428bf991adeef0852e8ec29239b692144
Dedy Yasriady <yasriady@gmail.com>
2023-09-16 08:08:21 +0700

    First commit

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -66,7 +66,7 @@ class WhatsAppInstance {
                 body,
                 instanceKey: key,
             })
-            .catch(() => {})
+            .catch(() => { })
     }
 
     async init() {
